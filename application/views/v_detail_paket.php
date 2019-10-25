34f6c43e4c18d477acb44add284d887be39058fd
Buat <almerfandriyanto98@gmail.com>
2019-10-25 16:23:05 +0700

    Detail Paket

--- application/views/v_detail_paket.php
+++ application/views/v_detail_paket.php
@@ -4,6 +4,166 @@
 			<div class="section-title-mid">
 				<h2>Detail Paket</h2>
 			</div>
+			<br>
+			<div class="col-lg-12 row">
+				<div class="col-lg-4">
+					<img class="d-satuan" src="<?php echo base_url(); ?>assets/images/paket_makanan.png" alt=""></div>
+				<div class="col-lg-6">
+					<h4>Paket Makanan Indonesia</h4>
+					<div class="rating">
+	                        	<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+								<span class="fa fa-star checked"></span>
+	                </div>
+	                <div>
+	                	Harga: Rp.12.000
+	                </div>
+	                <!-- <div class="tersedia">
+	                	Stock: <span style="color: green;">Tersedia</span>
+	                </div> -->
+	                <div>
+	                	Stock: <span style="color: red;">Tidak Tersedia</span>
+	                </div>
+
+	                <div class="col-lg-12 row" style="padding-top: 35px;">
+	                	<div class="col-lg-8">
+	                		<div class="form-group">
+						    <label for="exampleInputEmail1">Perkiraan Tamu</label>
+						    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Contoh: 100">
+						  </div>
+	                	</div>
+	                	<div class="col-lg-10 row">
+	                	  <div class="form-group col-lg-8">
+						    <label for="exampleInputEmail1">Catatan Tambahan (Opsional)</label>
+						    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Contoh: ayam tambah satu">
+						  </div>
+						  <div style="padding-top: 32px;">
+	                		<button type="button" class="btn btn-danger active">Add to cart</button>
+	                	  </div>
+	                	</div>
+	                </div>
+				</div>
+
+			</div>
+			<br>
+			<br>
+				<h4>Makanan & Minuman dalam Paket: </h4>
+			<br>
+
+				<div class="col-lg-12 row">
+					<div class="col-lg-4" style="padding-bottom: 40px;">	
+							 <div class="flip-card">
+							  <div class="flip-card-inner">
+							    <div class="flip-card-front">
+							      <img src="<?php echo base_url(); ?>assets/images/makanan/ayam_serundeng.png" style="width:298px;height:213px;">
+							    </div>
+							    <div class="flip-card-back">
+							      <h5>Ayam Serundeng</h5>
+							      <span style="color: black;">Informasi Gizi:</span>
+							      <div>	
+							      <ol style="color: black; padding-left: 20px;">
+  										<li>Vitamin A</li>
+ 										<li>Vitamin B</li>
+  										<li>Serat</li>
+  										<li>Protein</li>
+								  </ol>
+								  </div>
+							    </div>
+							  </div>
+							</div> 
+					</div>
+
+				<div class="col-lg-4" style="padding-bottom: 40px;">	
+							 <div class="flip-card">
+							  <div class="flip-card-inner">
+							    <div class="flip-card-front">
+							      <img src="<?php echo base_url(); ?>assets/images/makanan/sop_bening.png" style="width:298px;height:213px;">
+							    </div>
+							    <div class="flip-card-back">
+							      <h5>Sayur Sop Bening</h5>
+							      <span style="color: black;">Informasi Gizi:</span>
+							      <div>	
+							      <ol style="color: black; padding-left: 20px;">
+  										<li>Vitamin A</li>
+ 										<li>Vitamin B</li>
+  										<li>Serat</li>
+  										<li>Protein</li>
+								  </ol>
+								  </div>
+							    </div>
+							  </div>
+							</div> 
+					</div> 
+
+				<div class="col-lg-4" style="padding-bottom: 40px;">	
+							 <div class="flip-card">
+							  <div class="flip-card-inner">
+							    <div class="flip-card-front">
+							      <img src="<?php echo base_url(); ?>assets/images/makanan/gepuk.png" style="width:298px;height:213px;">
+							    </div>
+							    <div class="flip-card-back">
+							      <h5>Gepuk</h5>
+							      <span style="color: black;">Informasi Gizi:</span>
+							      <div>	
+							      <ol style="color: black; padding-left: 20px;">
+  										<li>Vitamin A</li>
+ 										<li>Vitamin B</li>
+  										<li>Serat</li>
+  										<li>Protein</li>
+								  </ol>
+								  </div>
+							    </div>
+							  </div>
+							</div> 
+					</div>
+
+					<div class="col-lg-4" style="padding-bottom: 40px;">	
+							 <div class="flip-card">
+							  <div class="flip-card-inner">
+							    <div class="flip-card-front">
+							      <img src="<?php echo base_url(); ?>assets/images/makanan/jus_mangga.png" style="width:298px;height:213px;">
+							    </div>
+							    <div class="flip-card-back">
+							      <h5>Jus Mangga</h5>
+							      <span style="color: black;">Informasi Gizi:</span>
+							      <div>	
+							      <ol style="color: black; padding-left: 20px;">
+  										<li>Vitamin A</li>
+ 										<li>Vitamin B</li>
+  										<li>Serat</li>
+  										<li>Protein</li>
+								  </ol>
+								  </div>
+							    </div>
+							  </div>
+							</div> 
+					</div>
+
+					<div class="col-lg-4" style="padding-bottom: 40px;">	
+							 <div class="flip-card">
+							  <div class="flip-card-inner">
+							    <div class="flip-card-front">
+							      <img src="<?php echo base_url(); ?>assets/images/makanan/jus_jambu.png" style="width:298px;height:213px;">
+							    </div>
+							    <div class="flip-card-back">
+							      <h5>Jus Jambu</h5>
+							      <span style="color: black;">Informasi Gizi:</span>
+							      <div>	
+							      <ol style="color: black; padding-left: 20px;">
+  										<li>Vitamin A</li>
+ 										<li>Vitamin B</li>
+  										<li>Serat</li>
+  										<li>Protein</li>
+								  </ol>
+								  </div>
+							    </div>
+							  </div>
+							</div> 
+					</div>
+
+				</div>
 
 		</div>
 	</section>
